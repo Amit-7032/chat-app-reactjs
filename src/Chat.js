43193811795aadccac78d2aa22821880309b3901
Amit-7032 <amitmishra4944@gmail.com>
2024-02-28 12:09:47 +0530

    update text area field and add some changes

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Message from './Message';
 
 const Chat = ({ messages }) => {
+  const [inputField, setInputField] = useState('');
+  const [chatMessage, setChatMessage] = useState([]);
+
+  const handleChange = (e) => {
+    setInputField(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (inputField.trim() !== '') {
+      setChatMessage([...chatMessage, inputField]);
+      setInputField('');
+    }
+  }
   return (
     <div className="chat">
      <h2>Chat Messages</h2>
       <div className="messages">
-        {messages.map((message, index) => (
-          <Message key={index} text={message.text} sender={message.sender} />
+        {chatMessage.map((message, index) => (
+          // <Message key={index} text={message.text} sender={message.sender} />
+          <div key={index}>
+            {message}
+          </div>
         ))}
       </div>
       <div>
-        <input type="text" placeholder="Type your message..." />
-        <button>Send</button>
+        <textarea type="text" placeholder="Type your message..." rows={5} value={inputField} onChange={handleChange} />
+        <button onClick={handleSubmit}>Send</button>
       </div>
     </div>
   );
